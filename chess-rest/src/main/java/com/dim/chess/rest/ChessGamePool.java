7f2c8b154a7c727800dd032de78a162ead200b2d
osvaldimar <osvaldimar1@gmail.com>
2017-06-19 20:00:53 -0300

    add in pom.xml dependency springframework web, change ChessGamePool to use PlayerConnectAI

--- chess-rest/src/main/java/com/dim/chess/rest/ChessGamePool.java
+++ chess-rest/src/main/java/com/dim/chess/rest/ChessGamePool.java
@@ -19,110 +19,115 @@ import com.chess.core.model.Difficulty;
 import com.chess.core.model.Player;
 import com.chess.core.service.ChessMultiplayerAI;
 import com.chess.core.service.ChessMultiplayerOnline;
-import com.dim.chess.ai.PlayerMachineAI;
+import com.dim.chess.ai.PlayerConnectAI;
 
 public class ChessGamePool {
-	
-	private Map<KeyUUIDChess, GameApplication> map = new ConcurrentHashMap<>();
-	private Queue<KeyClient> queue = new LinkedList<>();
-	
-	public GameApplication findGameApp(String uuid, String typePlayer) {
-		try{
-			if(TypePlayer.getEnum(typePlayer) == TypePlayer.W){
-				KeyUUIDChess key = new KeyUUIDChess(UUID.fromString(uuid), null);
-				Optional<KeyUUIDChess> findFirst = map.keySet().stream().filter(k -> k.equals(key)).findFirst();
-				if(findFirst.isPresent()) 
-					return map.get(findFirst.get());
-			}else if(TypePlayer.getEnum(typePlayer) == TypePlayer.B){
-				KeyUUIDChess key = new KeyUUIDChess(null, UUID.fromString(uuid));
-				Optional<KeyUUIDChess> findFirst = map.keySet().stream().filter(k -> k.equals(key)).findFirst();
-				if(findFirst.isPresent()) 
-					return map.get(findFirst.get());
+
+	private final Map<KeyUUIDChess, GameApplication> map = new ConcurrentHashMap<>();
+	private final Queue<KeyClient> queue = new LinkedList<>();
+
+	public GameApplication findGameApp(final String uuid, final String typePlayer) {
+		try {
+			if (TypePlayer.getEnum(typePlayer) == TypePlayer.W) {
+				final KeyUUIDChess key = new KeyUUIDChess(UUID.fromString(uuid), null);
+				final Optional<KeyUUIDChess> findFirst = this.map.keySet().stream().filter(k -> k.equals(key))
+						.findFirst();
+				if (findFirst.isPresent()) {
+					return this.map.get(findFirst.get());
+				}
+			} else if (TypePlayer.getEnum(typePlayer) == TypePlayer.B) {
+				final KeyUUIDChess key = new KeyUUIDChess(null, UUID.fromString(uuid));
+				final Optional<KeyUUIDChess> findFirst = this.map.keySet().stream().filter(k -> k.equals(key))
+						.findFirst();
+				if (findFirst.isPresent()) {
+					return this.map.get(findFirst.get());
+				}
 			}
-		} catch(NumberFormatException e) {
+		} catch (final NumberFormatException e) {
 			return null;
 		}
 		return null;
 	}
 
 	public ResponseClient joinSinglePlayerOnlineChessPool() {
-		UUID singleUuid = UUID.randomUUID();
-		KeyClient keyClientW = new KeyClient(singleUuid, TypePlayer.W);	
-		KeyClient keyClientB = new KeyClient(singleUuid, TypePlayer.B);			
-		ChessMultiplayerOnline chessOnline = new ChessMultiplayerOnline();
-		GameApplication game = chessOnline.startChess(new Player(TypePlayer.W), new Player(TypePlayer.B));
-		map.put(new KeyUUIDChess(keyClientW.getKey(), keyClientB.getKey()), game);
+		final UUID singleUuid = UUID.randomUUID();
+		final KeyClient keyClientW = new KeyClient(singleUuid, TypePlayer.W);
+		final KeyClient keyClientB = new KeyClient(singleUuid, TypePlayer.B);
+		final ChessMultiplayerOnline chessOnline = new ChessMultiplayerOnline();
+		final GameApplication game = chessOnline.startChess(new Player(TypePlayer.W), new Player(TypePlayer.B));
+		this.map.put(new KeyUUIDChess(keyClientW.getKey(), keyClientB.getKey()), game);
 		return this.buildResponseClientStartAdequateForThePlayer(keyClientW);
 	}
-	
+
 	public ResponseClient joinMultiPlayerOnlineChessPool() {
-		KeyClient keyClient = this.joinPoolGameMultiAddQueue();
-		while(true){
+		final KeyClient keyClient = this.joinPoolGameMultiAddQueue();
+		while (true) {
 			try {
-				if(this.isJoinPoolGameMulti(keyClient))
+				if (this.isJoinPoolGameMulti(keyClient)) {
 					break;
+				}
 				Thread.sleep(1000);
-			} catch (InterruptedException e) {
+			} catch (final InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		return buildResponseClientStartAdequateForThePlayer(keyClient);
+		return this.buildResponseClientStartAdequateForThePlayer(keyClient);
 	}
-	
-	public ResponseClient joinMultiPlayerAIOnlineChessPool(Difficulty difficultyAI) {
-		UUID singleUuid = UUID.randomUUID();
-		KeyClient keyClientW = new KeyClient(singleUuid, TypePlayer.W);	
-		KeyClient keyClientB = new KeyClient(singleUuid, TypePlayer.B);
-		ChessMultiplayerAI chessOnline = new ChessMultiplayerAI();
-		PlayerMode[] players = this.randomPlayerCommonAndAI(difficultyAI);
-		GameApplication game = chessOnline.startChess(players[0], players[1]);
-		map.put(new KeyUUIDChess(keyClientW.getKey(), keyClientB.getKey()), game);
+
+	public ResponseClient joinMultiPlayerAIOnlineChessPool(final Difficulty difficultyAI) {
+		final UUID singleUuid = UUID.randomUUID();
+		final KeyClient keyClientW = new KeyClient(singleUuid, TypePlayer.W);
+		final KeyClient keyClientB = new KeyClient(singleUuid, TypePlayer.B);
+		final ChessMultiplayerAI chessOnline = new ChessMultiplayerAI();
+		final PlayerMode[] players = this.randomPlayerCommonAndAI(difficultyAI);
+		final GameApplication game = chessOnline.startChess(players[0], players[1]);
+		this.map.put(new KeyUUIDChess(keyClientW.getKey(), keyClientB.getKey()), game);
 		return this.buildResponseClientStartAdequateForThePlayer(!players[0].isAI() ? keyClientW : keyClientB);
 	}
-	
-	private PlayerMode[] randomPlayerCommonAndAI(Difficulty difficultyAI){
-		PlayerMode[] players = {new Player(TypePlayer.W), new Player(TypePlayer.B)};
-		int r = new Random().nextInt(2);
-		players[r] = new PlayerMachineAI(players[r].getTypePlayer(), difficultyAI);
+
+	private PlayerMode[] randomPlayerCommonAndAI(final Difficulty difficultyAI) {
+		final PlayerMode[] players = { new Player(TypePlayer.W), new Player(TypePlayer.B) };
+		final int r = new Random().nextInt(2);
+		// players[r] = new PlayerMachineAI(players[r].getTypePlayer(),
+		// difficultyAI);
+		players[r] = new PlayerConnectAI(players[r].getTypePlayer(), difficultyAI);
 		return players;
 	}
-	
-	private ResponseClient buildResponseClientStartAdequateForThePlayer(KeyClient keyClient){
-		return new ResponseClient.Builder()
-				.status(ResponseChessboard.StatusResponse.START.toString())
-				.currentPlayer(TypePlayer.W.toString())
-				.turn(TypePlayer.W.toString())
-				.keyClientID(keyClient.getKey().toString())
-				.keyClientType(keyClient.getType().toString())
-				.build();
+
+	private ResponseClient buildResponseClientStartAdequateForThePlayer(final KeyClient keyClient) {
+		return new ResponseClient.Builder().status(ResponseChessboard.StatusResponse.START.toString())
+				.currentPlayer(TypePlayer.W.toString()).turn(TypePlayer.W.toString())
+				.keyClientID(keyClient.getKey().toString()).keyClientType(keyClient.getType().toString()).build();
 	}
-	
-	private synchronized KeyClient joinPoolGameMultiAddQueue(){
-		if(queue.isEmpty()){
-			KeyClient keyClient = new KeyClient(UUID.randomUUID(), TypePlayer.W);
-			queue.add(keyClient);
+
+	private synchronized KeyClient joinPoolGameMultiAddQueue() {
+		if (this.queue.isEmpty()) {
+			final KeyClient keyClient = new KeyClient(UUID.randomUUID(), TypePlayer.W);
+			this.queue.add(keyClient);
 			System.out.println("Thread name: " + Thread.currentThread().getName() + " - queue new W = " + keyClient);
 			return keyClient;
-		}else{
-			KeyClient keyClientW = queue.remove();
-			KeyClient keyClientB = new KeyClient(UUID.randomUUID(), TypePlayer.B);			
-			ChessMultiplayerOnline chessOnline = new ChessMultiplayerOnline();
-			GameApplication game = chessOnline.startChess(new Player(TypePlayer.W), new Player(TypePlayer.B));
-			map.put(new KeyUUIDChess(keyClientW.getKey(), keyClientB.getKey()), game);
+		} else {
+			final KeyClient keyClientW = this.queue.remove();
+			final KeyClient keyClientB = new KeyClient(UUID.randomUUID(), TypePlayer.B);
+			final ChessMultiplayerOnline chessOnline = new ChessMultiplayerOnline();
+			final GameApplication game = chessOnline.startChess(new Player(TypePlayer.W), new Player(TypePlayer.B));
+			this.map.put(new KeyUUIDChess(keyClientW.getKey(), keyClientB.getKey()), game);
 			System.out.println("Thread name: " + Thread.currentThread().getName() + " - queue new B = " + keyClientB);
 			return keyClientB;
 		}
 	}
-	
-	private synchronized boolean isJoinPoolGameMulti(KeyClient keyClient){
-		GameApplication findGameApp = this.findGameApp(keyClient.getKey().toString(), keyClient.getType().toString());
+
+	private synchronized boolean isJoinPoolGameMulti(final KeyClient keyClient) {
+		final GameApplication findGameApp = this.findGameApp(keyClient.getKey().toString(),
+				keyClient.getType().toString());
 		return findGameApp != null;
 	}
 
-	public int getTotalChessPool(){
+	public int getTotalChessPool() {
 		return this.map.size();
-	}	
-	public int getTotalChessQueuePending(){
+	}
+
+	public int getTotalChessQueuePending() {
 		return this.queue.size();
 	}
 }
